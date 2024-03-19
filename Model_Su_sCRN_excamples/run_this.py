17c3ec476abc12ea51bb7de36382f0217d0d37ff
SihaiYu <100762924+SihaiYu@users.noreply.github.com>
2024-03-19 19:12:02 +0800

    Add files via upload

---
+++ Model_Su_sCRN_excamples/run_this.py
@@ -0,0 +1,11 @@
+from surface_crns import SurfaceCRNQueueSimulator
+SurfaceCRNQueueSimulator.simulate_surface_crn("Fig11-D_LATCH_manifest.txt")
+
+#Fig3_token_fluctuation_manifest.txt
+#Fig4_crossing_wire_manifest.txt
+#Fig6_A_Ratchet_manifest.txt
+#Fig7_C_Ratchet_manifest.txt
+#Fig8_CJoin_manifest.txt
+#Fig9_2x2CJoin_manifest.txt
+#Fig10_CSequencer_manifest.txt
+#Fig11-D_LATCH_manifest.txt
